904a00e0a7ac169ca6537778c1d024a42d44a254
eugart <mr.artashkin@mail.ru>
2016-12-12 16:40:47 +0300

    css

--- bgb_webcontract/models.py
+++ bgb_webcontract/models.py
@@ -1,5 +1,7 @@
 import datetime
 import string
+import os
+import csv
 from random import choice
 
 from django.db import models
@@ -19,13 +21,34 @@ class Request(models.Model):
     it_manager_position = models.CharField(max_length=200, verbose_name='Должность заявителя')
     created_date = models.DateTimeField(auto_now=True)
     accepted = models.BooleanField(default=False)
-    rejection_reason = models.CharField(max_length=200, blank=True)
+    rejection_reason = models.CharField(max_length=200, blank=True, default='Заявка не обработана')
     department_id = models.ForeignKey(Department, on_delete=models.CASCADE,
                                       verbose_name='Факультет/Подразделение')
+    def create_csv(self):
+        if self.it_manager_email:
+            file_name = self.it_manager_email + '.csv'
+        else:
+            raise Exception('Email is not defined')
+        directory = os.getcwd() + '\\bgb_webcontract\generated_files\\'
+        if not os.path.exists(directory):
+            raise Exception('Directory is not exist')
+        else:
+            with open(directory + file_name, 'w', newline='') as f:
+                wr = csv.writer(f, delimiter=';')
+                header = ['ФИО', 'Должность', 'Ответственный за IT',
+                          'Логин', 'Пароль']
+                #wr.writerow(header)
+                for contract in self.contract_set.all():
+                    row = ['' for x in range(len(header))]
+                    row[0] = contract.full_name
+                    row[1] = contract.position
+                    row[2] = contract.request_id.it_manager_fullname
+                    row[3] = contract.login
+                    row[4] = contract.password
+                    wr.writerow(row)
 
     def __str__(self):
         return ' '.join([str(self.department_id), self.it_manager_fullname,])
-
 
 
 class Contract(models.Model):
@@ -39,40 +62,38 @@ class Contract(models.Model):
         return self.full_name
 
     def create_login(self):
-        siblings = Contract.objects.filter(request_id__department_id=self.request_id.department_id, login__isnull=False)
-        login_list = []
-        if siblings:
-            for c in siblings:
-                login_list.append(int(c.login))
-                self.login = str(sorted(login_list)[-1] + 1)
+        """Создание логина для УЖЕ СОХРАНЕННОГО в базе контракта
+
+         Сгенерированный логин - следующее по порядку значение
+         от самого большего (как число) существующего логина
+         для данного подразделения.
+        :return: Значение логина
+        """
+        # Сохранен ли объект в базе
+        if self.request_id:
+            # может у него уже есть логин?
+            if self.login:
+                return self.login
+            siblings = Contract.objects.filter(request_id__department_id=self.request_id.department_id,
+                                               login__isnull=False)
+            login_list = []
+            if siblings:
+                for c in siblings:
+                    if c.login:
+                        login_list.append(int(c.login))
+                        self.login = str(sorted(login_list)[-1] + 1)
+            else:
+                d_id = self.request_id.department_id_id
+                self.login = str(d_id) + '0001'
+            self.save()
+            return self.login
         else:
-            d_id = self.request_id.department_id_id
-            self.login = str(d_id) + '0001'
-        self.save()
-        return self.login
+            raise Exception("Can't create login, contract should be saved in DB")
 
     def create_password(self):
-        CHARS = string.ascii_lowercase + string.digits
-        LENGTH = 6
-        self.password = ''.join(choice(CHARS) for _ in range(LENGTH))
-        self.save()
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
+        if not self.password:
+            CHARS = string.ascii_lowercase + string.digits
+            LENGTH = 6
+            self.password = ''.join(choice(CHARS) for _ in range(LENGTH))
+            self.save()
+        return self.password
---
+++ lib/bgb_api.py
@@ -0,0 +1,227 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#print("Content-Type: text/plain\r\n")
+
+import requests
+import urllib
+import urllib.parse
+import csv
+import suds
+import logging
+import os
+import sys
+import xml.etree.ElementTree as ET
+from suds.client import Client
+from suds.transport.http import HttpAuthenticated
+
+
+#logging.basicConfig(level=logging.DEBUG)
+
+
+class BGBContract(object):
+
+
+    # id договора
+    id = 0
+    # номер договора
+    title = ""
+
+    def __init__(self, bgb_server='http://10.60.0.10:8080', bgb_login='icticket', bgb_password='ic05102015'):
+        self.bgb_server = bgb_server
+        self.bgb_login = bgb_login
+        self.bgb_password = bgb_password
+
+    def create_university_contract(self, fullname, department,
+                                   position, it_manager, login,
+                                   password, bgb_contract_template_id=28):
+        """Создание договора в БГБиллинге для сотрудника университета
+
+        :param bgb_contract_template_id: ID шаблона договора в БГБиллинге.
+        Проверяется в клиенте биллинга "Договор" -> "Шаблоны"
+        """
+        # try:
+        #     self.create_by_template(bgb_contract_template_id)  # Создание договора
+        # except Exception as e:
+        #     return False
+        # try:
+        #     self.set_fullname(self.id, fullname)  # ФИО
+        # except Exception as e:
+        #     # TODO Delete contract when create failed
+        #     return False
+        # try:
+        #     self.setLstParam(self.id, pid=9, value_id=department)  # Факультет
+        # except Exception as e:
+        #     # TODO Delete contract when create failed
+        #     return False
+        # try:
+        #     self.setStrParam(self.id, pid=11, value=position)  # Должность
+        # except Exception as e:
+        #     # TODO Delete contract when create failed
+        #     return False
+        # try:
+        #     self.setStrParam(self.id, pid=10, value=it_manager)  # Отвественный
+        # except Exception as e:
+        #     # TODO Delete contract when create failed
+        #     return False
+        # try:
+        #     self.setInetInfo(self.id, login=login, passwd=password)
+        # except Exception as e:
+        #     # TODO Delete contract when create failed
+        #     return False
+
+        self.create_by_template(bgb_contract_template_id)  # Создание договора
+        self.set_fullname(cid=self.id, fullname=fullname)  # ФИО
+        self.setLstParam(cid=self.id, pid=9, value_id=department)  # Факультет
+        self.setStrParam(cid=self.id, pid=11, value=position)  # Должность
+        self.setStrParam(cid=self.id, pid=10, value=it_manager)  # Отвественный
+        self.setInetInfo(cid=self.id, login=login, passwd=password)
+
+    def create_by_template(self,template_id):
+        """создание договора с использованием шаблона БГБиллинга
+
+        :param template_id: ID шаблона
+        """
+        self.template_id=template_id
+        payload = {
+            'user': self.bgb_login,  # логин
+            'pswd': self.bgb_password,  # пароль
+            'module': 'contract',  # модуль
+            'action': 'NewContract',  # действие
+            'sub_mode': '0',  # суб договорор
+            'pattern_id': template_id,
+            }
+        r = requests.get(self.bgb_server + "/bgbilling/executer", params=payload)
+        root = ET.fromstring(str(r.text))
+        for child in root:
+            self.id = child.attrib['id']
+            self.title = child.attrib['title']
+
+    def set_fullname(self, cid, fullname):
+        """ФИО договора
+
+        :param cid:
+        :param fullname:
+        """
+        payload = {
+            'user': self.bgb_login,
+            'pswd': self.bgb_password,
+            'module': 'contract',
+            'action': 'UpdateContractTitleAndComment',
+            'cid': cid,
+            'patid': '0',
+            }
+        r = requests.post(self.bgb_server + "/bgbilling/executer?comment=" + urllib.parse.quote(fullname),
+            params=payload)
+        root = ET.fromstring(str(r.text))
+        if root.attrib['status'] == 'error':
+            print("Error in method setFio: %s" % root.text)
+            raise "Error in method setFio: %s" % root.text
+
+
+    #Установка параметра типа строка(contract_parameter_type_1)принимает id договора,id параметра и значение параметра
+    def setStrParam(self, cid, pid, value):
+        payload = {
+            'user': self.bgb_login,#логин
+            'pswd': self.bgb_password,#пароль
+            'module': 'contract',#модуль
+            'action': 'UpdateParameterType1',#действие
+            'cid': cid,
+            'pid': pid
+            }
+        r=requests.post(self.bgb_server + "/bgbilling/executer?value=" +
+            urllib.parse.quote(value),
+            params=payload)
+        root = ET.fromstring(str(r.text))
+        if root.attrib['status'] == 'error':
+            print("Error in method setStrParam: %s" % root.text)
+            raise "Error in method setStrParam: %s" % root.text
+
+    def setLstParam(self, cid, pid, value_id):
+        """Установка параметра типа List
+
+        :param cid: ID договора
+        :param pid: ID параметра
+        :param value_id: значение параметра
+        """
+        print(pid, value_id)
+        payload = {
+            'user': self.bgb_login,#логин
+            'pswd': self.bgb_password,#пароль
+            'module': 'contract',#модуль
+            'action': 'UpdateListParam',#действие
+            'cid': cid,
+            'pid': pid,
+            'value': value_id
+            }
+        r = requests.get(self.bgb_server + "/bgbilling/executer", params=payload)
+        root = ET.fromstring(str(r.text))
+        if root.attrib['status'] == 'error':
+            print("Error in method setLstParam: %s" % root.text)
+            raise "Error in method setLstParam: %s" % root.text
+
+    def getInetInfo(self,cid):
+        """Возвращает данные клиента в модуле Inet
+
+        :param cid:
+        :return:
+        """
+        url = self.bgb_server + "/bgbilling/executer/ru.bitel.bgbilling.modules.inet.api/8/InetServService?wsdl"
+        t = HttpAuthenticated(username=self.bgb_login, password=self.bgb_password)
+        client = Client(url, transport=t)
+        try:
+            res=client.service.inetServList(cid)
+        except suds.WebFault as e:
+            print('Error in getInetInfo: %s' % (e.fault.detail.exception._cls))
+            sys.exit(-1)
+        else:
+            return res[0]
+
+    def setInetInfo(self, cid, **kwargs):
+        """Изменение параметров клиента в модуле Inet
+
+        :param cid:
+        :param kwargs:
+        """
+        url = self.bgb_server+"/bgbilling/executer/ru.bitel.bgbilling.modules.inet.api/8/InetServService?wsdl"
+        t = HttpAuthenticated(username=self.bgb_login, password=self.bgb_password)
+        client = Client(url, transport=t)
+        res = self.getInetInfo(cid)
+        if res!=None:
+            id=res._id
+            uname=res._uname
+            passwd=res._passw
+            typeId=res._typeId
+            if 'login' in kwargs.keys():
+                uname=str(kwargs['login'])
+            if 'passwd' in kwargs.keys():
+                passwd=kwargs['passwd']
+            serv={
+                "_cid": cid,
+                "_id": id,
+                "_passw": str(passwd),
+                "_typeId": typeId,
+                "_uname": uname,
+                "_title": uname
+            }
+            try:
+                res = client.service.inetServUpdate(serv, '', False, False, 0)
+            except suds.WebFault as e:
+                print('Error in setInetInfo: %s' % (e.fault.detail.exception._cls))
+                sys.exit(-1)
+            else:
+                print('SetInetInfo:success')
+        else:
+            print('setInetInfo: Error while call getInetInfo()')
+            sys.exit(-1)
+
+
+
+if __name__ == '__main__':
+    contract = BGBContract()
+    directory = os.getcwd()
+    with open(directory + '\\dep-study@adm.mrsu.ru.csv', 'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+            if row[1]!="":
+                print(row[0],row[1],row[2],row[3],row[4])
+                contract.create_university_contract(28)
