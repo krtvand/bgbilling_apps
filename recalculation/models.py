a52bd1a2b4c08bf262e2d7ad06a6556bc6187aa1
krtvand <krtvand@gmail.com>
2016-12-30 09:05:27 +0300

    fix fields blanks

--- recalculation/models.py
+++ recalculation/models.py
@@ -7,12 +7,11 @@ from random import choice
 from django.db import models
 import xlwt
 
+
 class Request(models.Model):
-	fullname = models.CharField(max_length=100, verbose_name='ФИО заявителя полностью')
-	contract_number = models.CharField(max_length=20,verbose_name='Номер договора')
-	date_begin = models.DateTimeField(verbose_name='Дата начала (гггг-мм-дд)')
-	date_end = models.DateTimeField(verbose_name='Дата окончания (гггг-мм-дд)')
-	created_date = models.DateTimeField(auto_now=True, verbose_name='Дата создания заявки')
-	comment = models.CharField(max_length=100, verbose_name='Комментарий оператора')
-
-
+    fullname = models.CharField(max_length=100, verbose_name='ФИО заявителя полностью', blank=True, null=True)
+    contract_number = models.CharField(max_length=20, verbose_name='Номер договора')
+    date_begin = models.DateTimeField(verbose_name='Дата начала (гггг-мм-дд)')
+    date_end = models.DateTimeField(verbose_name='Дата окончания (гггг-мм-дд)')
+    created_date = models.DateTimeField(auto_now=True, verbose_name='Дата создания заявки')
+    comment = models.CharField(max_length=100, verbose_name='Комментарий оператора', blank=True, null=True)
